6bbb533bd7133e88aff3b1bd70f2b5b2ca78678d
Youssef-Wahba <y.husseinwahba@gmail.com>
2022-11-29 14:04:16 +0200

    project base

---
+++ src/fawryDemo/Fawry.java
@@ -0,0 +1,10 @@
+package fawryDemo;
+
+public class Fawry {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author youssef
+ *
+ */
+module FawryDemo {
+}
