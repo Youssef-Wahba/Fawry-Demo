27b2822d4620aaa431b8ada3adf84a65fcadcfb3
Youssef-Wahba <Y.HusseinWahba@gmail.com>
2022-12-31 04:54:42 +0200

    project base

---
+++ src/test/java/com/example/fawrydemo/FawryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fawrydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FawryDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
