ff619147f13e09d0a99f8a7280b3b37a1b077e35
Youssef-Wahba <Y.HusseinWahba@gmail.com>
2022-12-31 05:08:31 +0200

    base project

---
+++ src/main/java/com/example/fawrydemo/FawryDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.fawrydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FawryDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FawryDemoApplication.class, args);
+    }
+
+}
